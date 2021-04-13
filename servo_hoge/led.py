2870863d7eee505b1f036cea73289aa2faf70c26
thoth <sbb03104@edu.osakafu-u.ac.jp>
2021-04-13 12:22:47 +0900

    ?

--- servo_hoge/led.py
+++ servo_hoge/led.py
@@ -31,7 +31,7 @@ class PikaPikaLed(Node):
     def led_callback(self, msg):
         self.pi = pigpio.pi()
         self.pi.set_node(self.SERVO_PIN, pigpio.OUTPUT)
-        if msg % 2 == 0:
+        if int(msg) % 2 == 0:
             self.pi.write(self.SERVO_PIN, 0)
             self.get_logger().info('led: 0')
         else:
