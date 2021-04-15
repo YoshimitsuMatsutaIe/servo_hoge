6fab48e7ec354db02ff488fc7c3e02ca2ba5dca1
thoth <sbb03104@edu.osakafu-u.ac.jp>
2021-04-15 17:16:12 +0900

    joj

--- servo_hoge/servo.py
+++ servo_hoge/servo.py
@@ -5,29 +5,32 @@ from std_msgs.msg import Int16
 import time
 import pigpio
 
-#SERVO_PIN = 23
 
-PULSE_MIN = 500 + 10
-PULSE_MAX = 2500 - 10
+PULSE_MIN = 500
+PULSE_MAX = 2500
 
-SERVO_PIN_1 = 24
-SERVO_PIN_2 = 23
+SERVO_PIN_1 = 17
+SERVO_PIN_2 = 27
+SERVO_PIN_3 = 22
+SERVO_PIN_4 = 23
+SERVO_PIN_5 = 24
 
 def pulse(ANGLE):
     """ANGLE[degree]からpwnパルス値を計算"""
     z = (ANGLE / 180) * (PULSE_MAX - PULSE_MIN) + PULSE_MIN
     return int(z)
 
+
 class ServoNode(Node):
     
     def __init__(self, SERVO_PIN):
         super().__init__("servo")
         self.SERVO_PIN = SERVO_PIN
-        self.init_motor()
+        #self.init_motor()
         self.create_subscription(
-            Int16, 
-            "countup", 
-            self.servo_callback, 
+            Int16,
+            "countup",
+            self.servo_callback,
             10,
             )
     
@@ -35,7 +38,6 @@ class ServoNode(Node):
         self.pi = pigpio.pi()
         self.pi.set_mode(self.SERVO_PIN, pigpio.OUTPUT)
         self.pi.set_servo_pulsewidth(self.SERVO_PIN, 1000)
-        self.get_logger().info('servoの初期化です')
     
     def servo_callback(self, msg):
         """ANGLEにINT16？の信号がくる？"""
@@ -48,38 +50,74 @@ class ServoNode(Node):
         self.get_logger().info('servo_exec: %d' % msg.data)
 
 
-# def main():
-#     rclpy.init()
-#     executor = SingleThreadedExecutor()
+def main_2():
+    """2個だけ同時実行"""
+    rclpy.init()
+    executor = SingleThreadedExecutor()
     
-#     ## ノードをインスタンス化しexecutorにadd
-#     # 1個目
-#     servo_1 = ServoNode(SERVO_PIN_1)
-#     executor.add_node(servo_1)
+    ## ノードをインスタンス化しexecutorにadd
+    # 1個目
+    servo_1 = ServoNode(SERVO_PIN_1)
+    executor.add_node(servo_1)
     
-#     # # 2個目
-#     # servo_2 = ServoNode(SERVO_PIN_2)
-#     # executor.add_node(servo_2)
+    # # 2個目
+    # servo_2 = ServoNode(SERVO_PIN_2)
+    # executor.add_node(servo_2)
     
-#     try:
-#         executor.spin()  # 無限ループ？
-#     except KeyboardInterrupt:  # Ctrl-C
-#         pass
+    try:
+        executor.spin()  # 無限ループ
+    except KeyboardInterrupt:  # Ctrl-C
+        pass
     
-#     ## ノード殺す
-#     executor.shutdown()
-#     servo_1.destroy_node()
-#     #servo_2.destroy_node()
-#     rclpy.shutdown()
+    ## ノード殺す
+    executor.shutdown()
+    servo_1.destroy_node()
+    #servo_2.destroy_node()
+    rclpy.shutdown()
 
 
+def main_n():
+    """n個同時実行"""
+    rclpy.init()
+    executor = SingleThreadedExecutor()
+    
+    ## ノードをインスタンス化しexecutorにadd
+    #servo_1 = ServoNode(SERVO_PIN_1)
+    #servo_2 = ServoNode(SERVO_PIN_2)
+    servo_3 = ServoNode(SERVO_PIN_3)
+    servo_4 = ServoNode(SERVO_PIN_4)
+    servo_5 = ServoNode(SERVO_PIN_5)
 
-def main():
+    #executor.add_node(servo_1)
+    #executor.add_node(servo_2)
+    executor.add_node(servo_3)
+    executor.add_node(servo_4)
+    executor.add_node(servo_5)
+    
+    
+    try:
+        executor.spin()  # 無限ループ
+    except KeyboardInterrupt:  # Ctrl-C
+        pass
+    
+    ## ノード終了
+    executor.shutdown()
+
+    #servo_1.destroy_node()
+    #servo_2.destroy_node()
+    servo_3.destroy_node()
+    servo_4.destroy_node()
+    servo_5.destroy_node()
+
+    rclpy.shutdown()
+
+
+def main_1():
     rclpy.init()
     node = ServoNode(SERVO_PIN_1)
 
     try:
-        rclpy.spin(node)  # 無限ループ？
+        rclpy.spin(node)  # 無限ループ
     except KeyboardInterrupt:  # Ctrl-C
         pass
     
@@ -91,4 +129,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main_n()
